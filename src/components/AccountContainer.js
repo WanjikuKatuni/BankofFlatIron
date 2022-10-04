2656d67cbb826f67a131cb102b6e828a60074763
wanjiku <annritawanjiku77@gmail.com>
2022-10-04 19:20:58 +0300

    fetch transactions

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
+
+  const [transactions, setTransactions] = useState([])
+
+  // fetch from dbjson
+  function getTransactions(){
+    fetch("http://localhost:8001/transactions")
+    .then((response)=> response.json())
+    .then(setTransactions)
+  }
+  
   return (
     <div>
       <Search />
